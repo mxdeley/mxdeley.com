504ab48966888a333867b33caf130cb45a9d77b0
Dan <dev@madeleydesignstudio.org>
2024-01-02 20:45:30 +0000

    updated cv

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 export default function CV() {
   return (
-    <main className="flex min-h-screen flex-col items-center text-sm max-w-4xl mx-auto p-20 space-y-10">
+    <main className="flex min-h-screen flex-col items-center max-w-4xl mx-auto p-20 space-y-10">
       <div className="flex items-center justify-between w-full">
         <div className="w-2/3 flex flex-col space-y-4">
-          <h1 className="font-bold text-lg">Daniel Madeley</h1>
+          <h1 className="font-bold text-xl">Daniel Madeley</h1>
           <div>
             <p className="text-stone-400">
               Experienced engineer working in tech and construction. Skilled in
@@ -79,8 +79,8 @@ export default function CV() {
       </div>
       <div className="flex justify-start w-full text-stone-400">
         <div>
-          <h2 className="text-md font-bold text-stone-50">About</h2>
-          <p>
+          <h2 className="text-lg font-bold text-stone-50">About</h2>
+          <p className="text-sm">
             With more than four years of experience as a freelance web
             developer, I&apos;ve honed my skills at my own design studio{" "}
             <Link href="#" className="text-stone-50">
@@ -103,18 +103,19 @@ export default function CV() {
       <div className="flex justify-start w-full text-stone-400">
         <div>
           <h2 className="text-md font-bold text-stone-50">Hobbies</h2>
-          <p>• MMA</p>
-          <p>• Football</p>
+          <p className="text-sm">• MMA</p>
+          <p className="text-sm">• Football</p>
+          <p className="text-sm">• Fishing</p>
         </div>
       </div>
 
       <div className="flex justify-start w-full">
         <div className="space-y-2">
-          <h2 className="font-bold">Work Experience</h2>
+          <h2 className="font-bold text-lg">Work Experience</h2>
           <div>
-            <h3>Austin Partnership</h3>
-            <h4 className="text-stone-400">2022-Current</h4>
-            <p className="text-stone-400">
+            <h3 className="font-bold">Austin Partnership</h3>
+            <h4 className="text-stone-400 text-sm">2022-Current</h4>
+            <p className="text-stone-400 text-sm">
               I have designed steel, concrete, masonry, timber and aluminium
               structures to the Eurocodes. I am experienced in Tekla Tedds,
               Structural Designer, StaTiCa, Fisher Fixperience and Hilti. Hand
@@ -122,10 +123,11 @@ export default function CV() {
               and Gmesh.
             </p>
           </div>
+
           <div>
-            <h3>madeleydesignstudio</h3>
-            <h4 className="text-stone-400">2021-Current</h4>
-            <p className="text-stone-400">
+            <h3 className="font-bold">madeleydesignstudio</h3>
+            <h4 className="text-stone-400 text-sm">2021-Current</h4>
+            <p className="text-stone-400 text-sm">
               I have successfully worked on a diverse range of web development
               projects for my esteemed client base. My primary focus has been
               leveraging the power of Next.js as a full-stack framework.
@@ -135,11 +137,40 @@ export default function CV() {
               to my clients.
             </p>
           </div>
+          <div className="max-w-2xl flex flex-col justify-end space-y-2 ">
+            <div>
+              <h5 className="text-sm font-semibold">Infinitim</h5>
+              <h4 className="text-stone-400 text-sm">2021-2023</h4>
+              <p className="text-stone-400 text-sm">
+                As a result of my experience with the Fungible Cabin, I was
+                inspired to establish my own digital marketing agency. I took
+                pleasure in crafting marketing websites for clients based in
+                Derby. My tenure at Infinitim provided me with an opportunity to
+                expand my expertise in various areas such as digital marketing,
+                web development, sales, and design. Additionally, I gained
+                proficiency in utilizing tools like Figma, Midjourney, and
+                Blender.
+              </p>
+            </div>
+            <div>
+              <h5 className="text-sm font-semibold">The Fungible Cabin</h5>
+              <h4 className="text-stone-400 text-sm">2021-2022</h4>
+              <p className="text-stone-400 text-sm">
+                During the NFT boom in 2021, two friends and I co-created
+                &apos;The Fungible Cabin&apos;. My role initially involved
+                working as a designer with Adobe Illustrator and Photoshop.
+                Subsequently, I ventured into solidity development, which
+                introduced me to the realm of web development and humbled me in
+                understanding its intricacies. This experience marked the
+                beginning of my journey as a web developer.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex justify-start w-full">
         <div>
-          <h2 className="text-md font-bold">Education</h2>
+          <h2 className="text-lg font-bold">Education</h2>
           <h3>Leeds Beckett</h3>
           <h4 className="text-stone-400">2019-2022</h4>
           <p className="text-stone-400">
@@ -149,7 +180,7 @@ export default function CV() {
         </div>
       </div>
       <div>
-        <p className="text-stone-400">
+        <p className="text-stone-400 text-sm">
           Explore my insightful{" "}
           <Link href="#" className="text-stone-50">
             blogs

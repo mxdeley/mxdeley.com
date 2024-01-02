1862c2cfcc0f65021c14d1a65ba6e770e5d959bb
Dan <dev@madeleydesignstudio.org>
2024-01-02 18:43:38 +0000

    edited side bar and resource pages

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,7 +1,17 @@
+import { ResourceCard } from "@/components/ResourceCard";
+
 export default function Resources() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <h1>name: Daniel Madeley</h1>
+    <main className="flex min-h-screen flex-col items-center text-sm max-w-4xl mx-auto p-20 space-y-10">
+      <div className="text-stone-400 text-center">
+        <h2>
+          Hey, check out my favourite resources. I either use these daily or
+          plan on using them in the future.
+        </h2>
+        <p>(Greyscale items are currently not used in my daily stack)</p>
+      </div>
+
+      <ResourceCard />
     </main>
   );
 }
---
+++ components/ResourceCard.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import DotPattern from "@/components/magicui/dot-pattern";
+import Link from "next/link";
+
+const Resources = [
+  { id: 1, href: "/resources/web_tech_stack", title: "Web Stack" },
+  { id: 1, href: "/resources/python_libraries", title: "Python Libraries" },
+  { id: 3, href: "/resources/productivity_stack", title: "Productivity Stack" },
+];
+
+export const ResourceCard = () => {
+  return (
+    <>
+      {Resources.map((resource) => {
+        return (
+          <Link
+            href={resource.href}
+            className="w-full flex items-center justify-center"
+            key={resource.id}
+          >
+            <div className="relative flex h-[3rem] w-[20rem] items-center justify-center overflow-hidden rounded-lg border bg-background p-20 shadow-2xl">
+              <p className="z-10 whitespace-pre-wrap text-center text-2xl font-medium tracking-tighter text-black dark:text-white">
+                {resource.title}
+              </p>
+              <DotPattern
+                className={cn(
+                  "[mask-image:radial-gradient(200px_circle_at_center,white,transparent)]"
+                )}
+              />
+            </div>
+          </Link>
+        );
+      })}
+    </>
+  );
+};

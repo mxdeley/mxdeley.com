cc86708b1c9d59572a3b564da56d61e5829cde7b
Dan <dev@madeleydesignstudio.org>
2024-01-16 21:40:19 +0000

    nothing added, i was practising ghost

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,6 +1,20 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 
 const Project = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await import("@/content/projects/projects.json");
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+
   return <div>Project</div>;
 };
 
--- app/resources/productivity_stack/page.tsx
+++ app/resources/productivity_stack/page.tsx
@@ -1,7 +1,7 @@
 export default function PythonLibraries() {
   return (
     <main className="flex min-h-screen flex-col items-center text-sm max-w-4xl mx-auto p-20 space-y-10">
-      snax
+      <div>Today I learned how to setup ghost</div>
     </main>
   );
 }

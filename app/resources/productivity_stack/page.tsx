6bce6332cc2931180d0950d6949d4a915ab2e580
Dan <dev@madeleydesignstudio.org>
2024-01-18 21:34:00 +0000

    added productivity stack

--- app/resources/productivity_stack/page.tsx
+++ app/resources/productivity_stack/page.tsx
@@ -1,7 +1,12 @@
 export default function PythonLibraries() {
   return (
     <main className="flex min-h-screen flex-col items-center text-sm max-w-4xl mx-auto p-20 space-y-10">
-      <div>Today I learned how to setup ghost</div>
+      <div>
+        <h2>Notion</h2>
+        <h2>Notion Calendar</h2>
+        <h2>Rize</h2>
+        <h2>Obsidian</h2>
+      </div>
     </main>
   );
 }
